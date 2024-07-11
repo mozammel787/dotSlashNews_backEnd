19c2d8d9d0e14a6685512f8240529897b1538cad
mozammel787 <mhfaryanemon@gmail.com>
2024-07-11 22:15:44 +0600

    fixed issue

--- index.js
+++ index.js
@@ -102,6 +102,7 @@ async function run() {
     app.get("/user", async (req, res) => {
       const data = user.find();
       const result = await data.toArray();
+      console.log(result);
       res.send(result);
     });
 
